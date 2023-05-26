143d6db943f8f93ffa3db790e128904a42f2a4fe
PIEspace <134577378+PIEspace@users.noreply.github.com>
2023-05-26 07:43:14 +0530

    traffic .py

---
+++ traffic .py
@@ -0,0 +1,15 @@
+# define the smart traffic system using if-else statement
+
+# define the age 
+# input from the user 
+age = int (input("enter your age "))
+# define the if-else statement
+
+if(age > 18):
+    print ("you can drive it sir have a nice day ")
+
+elif(age < 18 ):
+    print ("you can not drive it sir have a nice day")
+
+else :
+    print ("ERROR")
